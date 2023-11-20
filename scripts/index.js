4837a844afb7cdc2817f28c28cc85dfac856db66
Tomas Alves de Souza <tomas.alvesdesouza@datacom.co.nz>
2023-11-21 00:26:30 +1300

    Finish displaying data, make app responsive, add input to chose amount of cards, add animation when loading

--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,66 @@
 document.addEventListener("DOMContentLoaded", () => {
   const containerElement = document.getElementById("container");
+  const button = document.getElementById("btn");
 
-  let data = [];
+  button.addEventListener("click", () => submitForm());
 
-  const fetchData = async () => {
-    const response = await fetch("https://randomuser.me/api/?results=3");
-    data = await response.json();
+  const fetchData = async (cardsQty) => {
+    try {
+      containerElement.innerHTML = "";
 
-    let card = null;
-    data.results.forEach((item) => {
-      console.log(item.name.first);
-      card = document.createElement("div");
-      card.classList.add("card");
-      card.innerHTML = `
-      <div class="photo-container">
-        <img src=${item.picture.large} alt="Card of ${item.name.first} ${item.name.last}"  class="photo"/>
-      </div>
-      <div class="card-item">
-        <p class="name">${item.name.title}.</p>
-        <p class="name">${item.name.first}</p>
-        <p class="name">${item.name.last}</p>
-      </div>
-      <div>
-        <p class="address">
-          ${item.location.street.number} 
-          ${item.location.street.name} - 
-          ${item.location.state} / 
-          ${item.location.country}
-        </p>
-      </div>
-      <div class="contact-details">
-        <p>Phone number: ${item.phone}</p>
-        <p>Email address: ${item.email}</p>
-      </div>
+      const response = await fetch(
+        `https://randomuser.me/api/?results=${cardsQty}`
+      );
+      data = await response.json();
+
+      data.results.forEach((item) => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.innerHTML = `
+        <div class="photo-container">
+          <img src=${item.picture.large} alt="Card of ${item.name.first} ${item.name.last}"  class="photo"/>
+        </div>
+        <div class='info-container'>
+          <div class="card-name">
+            <p>${item.name.title}.</p>
+            <p>${item.name.first}</p>
+            <p>${item.name.last}</p>
+          </div>
+          <div class="info-item">
+            <h5>Address:</h5>
+              <h6>${item.location.street.number} ${item.location.street.name} st</h6> 
+                <h6> ${item.location.state} / ${item.location.country}</h6> 
+          </div>
+            <div class="info-item">
+              <h5>Phone number: </h5>
+              <p>${item.phone}</p>
+            </div>
+            <div class="info-item">
+              <h5>Email address: </h5>
+              <p>${item.email}</p>
+            </div>
+        </div>
       `;
-
-      container.appendChild(card);
-    });
-    console.log(data.results);
-    return card;
+        containerElement.appendChild(card);
+      });
+    } catch (err) {
+      alert("ERROR: " + err);
+    }
   };
 
-  fetchData();
+  function submitForm() {
+    const cardsQty = document.getElementById("cards-qty").value;
+
+    if (cardsQty <= 0) {
+      console.log(cardsQty);
+      alert("Please select a number bigger than zero.");
+      return;
+    } else {
+      fetchData(cardsQty);
+    }
+
+    document.getElementById("cards-qty").value = "";
+  }
+
+  fetchData(3);
 });
